5a1a848c6decf9d7dada6d0888adbc5e5b834eb0
ariaramin <ariaramin24@gmail.com>
2021-04-28 10:16:41 +0430

    commited

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,149 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :class="typeof weather.main != 'undefined' && Math.round(weather.main.temp) <= 15 ? 'cold' : ''">
+    <main class="content">
+      <div class="search-box">
+        <input type="search" class="search" placeholder="Search Location..." v-model="location" @keypress.enter="FindWeather">
+      </div>
+      <div class="info" v-if="typeof weather.main != 'undefined'">
+        <div class="location-date">
+          <h1 class="location">{{ weather.name }}, {{ weather.sys.country }}</h1>
+          <p class="date">{{ Today }}</p>
+        </div>
+
+        <div class="temp">
+          <span>{{ Math.round(weather.main.temp) }}°</span>
+        </div>
+
+        <div class="weather">
+          <p>{{ weather.weather[0].main }}</p>
+          <h4>{{ Math.round(weather.main.temp_min) }}/ {{ Math.round(weather.main.temp_max) }}°C</h4>
+        </div>
+      </div>
+      <div class="warning" v-else>
+        <h1>Search Location</h1>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      api_key: '8678eb8247c7f2cf6fe0e7d2bc5d66a6',
+      location: '',
+      weather: {}
+    }
+  },
+  methods: {
+    FindWeather(){
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.location}&units=metric&appid=${this.api_key}`)
+        .then(result  => result.json()).then(res => this.weather = res)
+    }
+  },
+  computed: {
+    Today(){
+      let date = new Date()
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+      let day = days[date.getDay()]
+      let month = months[date.getMonth()]
+      let year = date.getFullYear()
+
+      return `${day}, ${month} ${date.getDate()}, ${year}`
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+*{
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  transition: 0.4s;
 }
+
+#app {
+  text-align: center;
+  background: url('assets/warm-bg.jpeg') no-repeat bottom;
+  background-size: cover;
+  height: 100vh;
+  color: #FFFFFF;
+  text-shadow: 3px 4px 20px rgba(0, 0, 0, 0.25);
+}
+
+#app.cold{
+  background: url('assets/cold-bg.jpg') no-repeat bottom;
+  background-size: cover;
+}
+
+.content{
+  height: 100vh;
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5));
+}
+
+.search-box{
+  padding: 1.2rem;
+}
+
+.search-box .search{
+  width: 100%;
+  outline: none;
+  border: none;
+  padding: .5rem;
+  height: 40px;
+  background: #FFFFFF;
+  box-shadow: 3px 4px 20px rgba(0, 0, 0, 0.25);
+  border-radius: 0 10px;
+}
+
+.search::placeholder{
+  line-height: 25px;
+  color: #7E7E7E;
+}
+
+.info, .warning{
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  width: max-content;
+}
+
+.temp{
+  margin: 3rem auto;
+}
+
+.location-date .location{
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
+}
+
+.location-date .date{
+  font-size: 1.2rem;
+}
+
+.temp span{
+  font-size: 4rem;
+  position: relative;
+}
+
+.temp span::before{
+  content: 'C';
+  position: absolute;
+  right: -25px;
+  bottom: 5px;
+  font-size: 2.5rem;
+}
+
+.weather p{
+  font-size: 3rem;
+  margin-bottom: 1rem;
+}
+
 </style>
